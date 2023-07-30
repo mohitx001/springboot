e43a8271fa17122023906b64371a2021249fd276
mohit <mohit>
2023-07-31 03:55:07 +0530

    removed all

--- HelloWorldController.java
+++
@@ -1,9 +0,0 @@
-// File: HelloWorldController.java
-@RestController
-public class HelloWorldController {
-    @GetMapping("/")
-    public String hello() {
-        return "Hello, Dockerized Spring Boot App!";
-    }
-}
-
--- SpringBootDockerApp.java
+++
@@ -1,8 +0,0 @@
-// File: SpringBootDockerApp.java
-@SpringBootApplication
-public class SpringBootDockerApp {
-    public static void main(String[] args) {
-        SpringApplication.run(SpringBootDockerApp.class, args);
-    }
-}
-
